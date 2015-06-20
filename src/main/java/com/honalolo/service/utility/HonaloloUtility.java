00d6edf9bc2d1b1649e8b17978c42b6767fc0fec
unknown <dhrubajyotib@SCS02-LT-046.pune.smartek21.st21>
2015-06-20 18:22:54 +0530

    Added logger log4j

--- src/main/java/com/honalolo/service/utility/HonaloloUtility.java
+++ src/main/java/com/honalolo/service/utility/HonaloloUtility.java
@@ -7,6 +7,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 
@@ -18,7 +19,7 @@ import org.springframework.stereotype.Component;
  */
 public class HonaloloUtility {
 	
-	
+	public static final Logger logger = Logger.getLogger(HonaloloUtility.class);
 	private ResponseBuilder responseBuilder;
 	
 	/**
@@ -28,6 +29,7 @@ public class HonaloloUtility {
 	 * Builds the response for http requests.
 	 */
 	public Response buildServiceResponse(Object object){
+		logger.debug("Building the Webservice response");
 		this.responseBuilder = Response.ok();
 		this.responseBuilder.entity(object);
 
@@ -45,10 +47,10 @@ public class HonaloloUtility {
 	 */
 	public String callWebServices(String serverPath, String serviceName, HashMap<String, String> params){
 		
+		
+		logger.info("Calling Service --> "+serverPath+serviceName);
 		WebClient webClient =null;
-		
 		webClient = WebClient.create(serverPath);
-		
 		if(null !=params){
 			for(Entry<?,?> entry : params.entrySet()){
 				webClient.query(entry.getKey().toString(), entry.getValue().toString());
