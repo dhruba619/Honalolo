c10395ab73b752c08087e319b02b8956e1f849da
dhruba619 <dhruba619@gmail.com>
2015-06-28 17:30:55 +0530

    Implemented saveDocument

--- honalolo-dao/src/main/java/com/honalolo/dao/impls/DaoOperationsImpl.java
+++ honalolo-dao/src/main/java/com/honalolo/dao/impls/DaoOperationsImpl.java
@@ -4,17 +4,30 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.bson.Document;
+
+import com.google.gson.Gson;
 import com.honalolo.dao.Interface.IDaoOperations;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class DaoOperationsImpl implements IDaoOperations {
 
 	@Inject
 	private DbConnection databaseConnection;
 	
+	@Inject
+	private Gson gson;
+	
 	@Override
-	public void saveDocument(Object object) {
-		// TODO Auto-generated method stub
-
+	public void saveDocument(Object object, String collectionName) {
+		//The database name is to read from a database.properties
+		MongoDatabase database = this.databaseConnection.getDatabaseConnection("testDb");
+		MongoCollection<Document> collection = database.getCollection(collectionName);
+		String jsonData = gson.toJson(object);
+		Document document = Document.parse(jsonData);
+		collection.insertOne(document);
+		
 	}
 
 	@Override
